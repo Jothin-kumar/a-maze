a50c18d2b8ed1046876fd336497325ef8529f8b4
Jothin Kumar <contact@jothin.tech>
2024-04-01 04:37:50 +0530

    game over screen x-large text for "Game Over"

--- script/game-over.js
+++ script/game-over.js
@@ -6,6 +6,6 @@ function gameOver(steps, startTime) {
     score = (score*100).toFixed(2);
     document.getElementById("main").style.display = "none";
     document.getElementById("controls").style.display = "none";
-    document.getElementById("game-over").innerText = `Game Over\nScore: ${score}%\nTime taken: ${((new Date().getTime() - startTime)/1000).toFixed(2)}s`;
+    document.getElementById("game-over").innerHTML = `<strong style="font-size: x-large">Game Over</strong><br>Score: ${score}%<br>Time taken: ${((new Date().getTime() - startTime)/1000).toFixed(2)}s`;
     document.getElementById("game-over-parent").style.display = "block";
 }
