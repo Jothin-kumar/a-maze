8168909660627b44265feee0d9b4dc6eb2dee55e
Jothin Kumar <contact@jothin.tech>
2024-03-31 19:09:57 +0530

    fullscreen

--- script/main.js
+++ script/main.js
@@ -4,3 +4,9 @@ function revealAnswer() {
      });
 }
 setTimeout(construct, 100)
+window.addEventListener("touchstart", () => {
+    document.body.requestFullscreen();
+})
+window.addEventListener("click", () => {
+    document.body.requestFullscreen();
+})
