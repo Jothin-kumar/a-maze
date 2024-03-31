2a4ce6c0f2ec7eb999466892f1e3583eabbb7c33
Jothin Kumar <contact@jothin.tech>
2024-03-31 20:44:37 +0530

    improvements

--- script/main.js
+++ script/main.js
@@ -1,7 +1,12 @@
 function revealAnswer() {
+    Object.values(window.mazeSquares).forEach((dot) => {
+        dot.setColor("black");
+    })
     mp.path.forEach((dot) => {
-        dot.setColor("rgba(0, 0, 255, .25)");
-     });
+        dot.setColor("rgba(0, 255, 0, .25)");
+    });
+    mp.start.setColor("green");
+    mp.end.setColor("red");
 }
 setTimeout(construct, 100)
 window.addEventListener("touchstart", () => {
