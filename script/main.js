ad634035fb94eced02b8924a4b4d26e772fc6b14
Jothin Kumar <contact@jothin.tech>
2024-04-01 21:12:28 +0530

    hide onscreen nav after revealing answer

--- script/main.js
+++ script/main.js
@@ -10,5 +10,6 @@ function revealAnswer() {
     mp.end.setColor("red");
     document.getElementById("reset-btn").style.display = "none"
     document.getElementById("reveal-answer").style.display = "none"
+    document.getElementById("onscreen-nav").classList.remove("show-onscreen-nav")
 }
 setTimeout(construct, 100)
