f7f2ad73ac3cbfe132d1ffe531f242f07fe958f6
Jothin Kumar <contact@jothin.tech>
2024-04-08 19:47:49 +0530

    display maze image in game over screen

--- script/game-over.js
+++ script/game-over.js
@@ -4,7 +4,9 @@ function gameOver(steps, startTime) {
         score = 1;
     }
     score = (score*100).toFixed(2);
-    document.getElementById("main").style.display = "none";
+    const main = document.getElementById("main");
+    document.getElementById("game-over-maze-parent").appendChild(main);
+    player.reset()
     document.getElementById("controls").style.display = "none";
     document.getElementById("game-over").innerHTML = `<strong style="font-size: x-large">Game Over</strong><br>Score: ${score}%<br>Time taken: ${((new Date().getTime() - startTime)/1000).toFixed(2)}s`;
     document.getElementById("game-over-parent").style.display = "block";
