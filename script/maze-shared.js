7fb9145e854d5314681fcdc1deb02454fff1b1f4
Jothin Kumar <contact@jothin.tech>
2024-04-09 15:52:46 +0530

    compress maze data

--- script/maze-shared.js
+++ script/maze-shared.js
@@ -1,24 +1,45 @@
-function exportMaze() {
-    const hiddenLines = Object.keys(window.lines).filter(key => window.lines[key].hidden);
-    const correctPathElems = mp.path.filter(sq => ![mp.start, mp.end].includes(sq));
-    const correctPathSquares = correctPathElems.map(sq => `${sq.x},${sq.y}`);
+// Format: start end correctPathSquares-hiddenLinesXExcess-hiddenLinesYExcess
+// (without space)
+// Every 2 consecutive alphabets represent a coordinate
+// for hiddenLines, coordinate is half
 
-    return JSON.stringify({
-        start: `${mp.start.x},${mp.start.y}`,
-        end: `${mp.end.x},${mp.end.y}`,
-        correctPathSquares: correctPathSquares,
-        hiddenLines: hiddenLines
-    })
+const alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+numToAlpha = (n) => alphabets[n-1];
+alphaToNum = (a) => alphabets.indexOf(a) + 1;
+coordToAlpha = (x, y) => numToAlpha(x) + numToAlpha(y);
+alphaToCoord = (alpha) => [alphaToNum(alpha[0]), alphaToNum(alpha[1])];
+
+
+function exportMaze() {
+    const correctPathElems = mp.path.filter(sq => ![mp.start, mp.end].includes(sq));
+    const hiddenLines = (Object.keys(window.lines).filter(k => window.lines[k].hidden).map(k => k.split(",").map(n => parseInt(n))));
+    const hiddenLinesXExcess = hiddenLines.filter(l => l[0] % 2 == 1);
+    const hiddenLinesYExcess = hiddenLines.filter(l => l[1] % 2 == 1);
+
+    let compressedData =  `${coordToAlpha(mp.start.x, mp.start.y)}${coordToAlpha(mp.end.x, mp.end.y)}`
+    compressedData += correctPathElems.map(sq => coordToAlpha(sq.x, sq.y)).join("")
+    compressedData += `-${hiddenLinesXExcess.map(l => coordToAlpha((l[0]-1)/2, l[1]/2)).join("")}`
+    compressedData += `-${hiddenLinesYExcess.map(l => coordToAlpha(l[0]/2, (l[1]-1)/2)).join("")}`
+    return compressedData;
 }
 
 function loadMazeFromShared(data) {
-    const maze = JSON.parse(data);
-    mp.start = window.mazeSquares[maze.start];
-    mp.end = window.mazeSquares[maze.end];
-    mp.path = maze.correctPathSquares.map(c => window.mazeSquares[c]);
+    mp.start = window.mazeSquares[alphaToCoord(data.slice(0, 2)).join(",")];
+    mp.end = window.mazeSquares[alphaToCoord(data.slice(2, 4)).join(",")];
 
-    for (let i = 0; i < maze.hiddenLines.length; i++) {
-        window.lines[maze.hiddenLines[i]].hide();
+    data = data.slice(4).split("-");
+
+    mp.path = [mp.start];
+    for (let i = 0; i < data[0].length; i += 2) {
+        mp.path.push(window.mazeSquares[alphaToCoord(data[0].slice(i, i+2)).join(",")]);
+    }
+    mp.path.push(mp.end);
+
+    for (let i = 0; i < data[1].length; i += 2) {
+        window.lines[`${2*alphaToNum(data[1][i])+1},${2*alphaToNum(data[1][i+1])}`].hide();
+    }
+    for (let i = 0; i < data[2].length; i += 2) {
+        window.lines[`${2*alphaToNum(data[2][i])},${2*alphaToNum(data[2][i+1])+1}`].hide();
     }
 
     postConstruct()
