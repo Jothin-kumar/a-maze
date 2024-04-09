dccdd48f08372183ad88c68239ff9c308fab225b
Jothin Kumar <contact@jothin.tech>
2024-04-09 16:01:55 +0530

    show error in case of invalid maze data

--- script/main.js
+++ script/main.js
@@ -1,7 +1,15 @@
 const sp = new URLSearchParams(window.location.search)
 if (sp.has("maze-data")) {
     setTimeout(() => {
-        loadMazeFromShared(sp.get("maze-data"))
+        try {
+            loadMazeFromShared(sp.get("maze-data"))
+        }
+        catch (e) {
+            console.error(e)
+            document.getElementById("loading").style.display = "none"
+            document.getElementById("error-msg").style.display = "block"
+            document.getElementById("error-msg").innerHTML = "<p style='color: red'>Invalid maze data</p><button style='width: 100%; background-color: black; color: white; padding: 5px; border: olive 1px solid; cursor: pointer' onclick='window.location = window.location.pathname'>New Maze</button>"
+        }
     }, 100)
 }
 else {
