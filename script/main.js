f33663ce1857fd87905c735b25247e6e8bc227db
Jothin Kumar <contact@jothin.tech>
2024-04-10 21:44:55 +0530

    redirect slower devices to maze collection

--- script/main.js
+++ script/main.js
@@ -7,14 +7,28 @@ if (sp.has("maze-data")) {
         catch (e) {
             console.error(e)
             document.getElementById("loading").style.display = "none"
-            document.getElementById("error-msg").style.display = "block"
-            document.getElementById("error-msg").innerHTML = "<p style='color: red'>Invalid maze data</p><button style='width: 100%; background-color: black; color: white; padding: 5px; border: olive 1px solid; cursor: pointer' onclick='window.location = window.location.pathname'>New Maze</button>"
+            document.getElementById("msg").style.display = "block"
+            document.getElementById("msg").innerHTML = "<p style='color: red'>Invalid maze data</p><button style='width: 100%; background-color: black; color: white; padding: 5px; border: olive 1px solid; cursor: pointer' onclick='window.location = window.location.pathname'>New Maze</button>"
         }
     }, 100)
 }
 else {
-    setTimeout(construct, 100)
+    a = new Date().getTime()
+    for (let i = 0; i < 10000000; i++) {i+1}
+    b = new Date().getTime()
+    if (b - a < 15) {
+        setTimeout(construct, 100)
+    }
+    else {
+        document.getElementById("loading").style.display = "none"
+        document.getElementById("msg").style.display = "block"
+        document.getElementById("msg").innerHTML = "<p>Redirecting to maze collection...</p>"
+        setTimeout(() => {
+            window.location.href = "https://mazes.jothin.tech"
+        }, 3000)
+    }
 }
+
 if (sp.has("share-url")) {
     const shareURL = sp.get("share-url");
     if (shareURL.startsWith("a-maze.jothin.tech/share?s=") || shareURL.startsWith("mazes.jothin.tech/@")) {
