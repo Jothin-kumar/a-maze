caf1532ef52af074b64c982769f12ee83f050f77
Jothin Kumar <contact@jothin.tech>
2024-04-13 19:15:11 +0530

    difficulty calculation

---
+++ script/difficulty.js
@@ -0,0 +1,24 @@
+function getDifficulty() {
+    var difficulty = 0;
+    function movableNeighbours(dot) {
+        const neighbors = [];
+        function handler(x, y) {
+            let potentialMovableNeighbor = window.mazeSquares[`${dot.x+x},${dot.y+y}`];
+            if (potentialMovableNeighbor && canMove(dot.x, dot.y, dot.x + x, dot.y + y)) {
+                neighbors.push(potentialMovableNeighbor);
+            }
+        }
+        [[1, 0], [-1, 0] ,[0, 1], [0, -1]].forEach(([x, y]) => handler(x, y));
+        return neighbors;
+    }
+    const correctPathList = Object.values(window.mp.path)
+    for (let i = 0; i < correctPathList.length; i++) {
+        let dot = correctPathList[i];
+        let neighbors = movableNeighbours(dot);
+        difficulty += neighbors.length - 2;
+    }
+    
+    difficulty *= Object.values(lines).filter(l => l.hidden).length / Object.values(lines).length;
+    difficulty = Math.round(difficulty);
+    return difficulty;
+}
