b613640d522a83b93786b6e4828eda6890c3980a
Jothin Kumar <contact@jothin.tech>
2024-04-18 12:50:03 +0530

    focus update

--- script/focus.js
+++ script/focus.js
@@ -1,8 +1,22 @@
-function focusElem(elem) {
+function elemInViewport(elem, sm) {
+    const rect = elem.getBoundingClientRect();
     const main = document.getElementById("main");
-    setTimeout(() => {
-        elem.scrollIntoView({block: "center", inline: "center"});
-    }, 200)
+    const mainRect = main.getBoundingClientRect();
+    return (
+        rect.top >= mainRect.top + sm &&
+        rect.left >= mainRect.left + sm &&
+        rect.bottom <= mainRect.bottom + sm &&
+        rect.right <= mainRect.right + sm
+    )
+}
+function focusElem(elem) {
+    if (!elemInViewport(elem, 100)) {
+        elem.style.scrollMargin = "100px";
+        elem.scrollIntoView();
+    }
+}
+function focusStart() {
+    focusElem(window.mp.start.elem);
     const locater = document.getElementById("locate-animation")
     setTimeout(() => {
         const elemRect = elem.getBoundingClientRect();
@@ -10,7 +24,4 @@ function focusElem(elem) {
         locater.style.left = `${(elemRect.left + elemRect.right - 200 - main.scrollLeft + elemRect.width)/2}px`;
         locater.classList.add("locate-animation");
     }, 1000)
-}
-function focusStart() {
-    focusElem(window.mp.start.elem);
 }
