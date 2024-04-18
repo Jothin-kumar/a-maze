b20095e7e55a6b990c087c0678a7603a848dcbbe
Jothin Kumar <contact@jothin.tech>
2024-04-18 13:25:01 +0530

    remove locater

--- script/focus.js
+++ script/focus.js
@@ -17,13 +17,6 @@ function focusElem(elem) {
 }
 function focusStart() {
     focusElem(window.mp.start.elem);
-    const locater = document.getElementById("locate-animation")
-    setTimeout(() => {
-        const elemRect = window.mp.start.elem.getBoundingClientRect();
-        locater.style.top = `${(elemRect.top + elemRect.bottom - 200 - main.scrollTop + elemRect.height)/2}px`;
-        locater.style.left = `${(elemRect.left + elemRect.right - 200 - main.scrollLeft + elemRect.width)/2}px`;
-        locater.classList.add("locate-animation");
-    }, 1000)
 }
 
 function unfocusControls() {
