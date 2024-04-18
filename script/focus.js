06431e891752fe1fae3395c44318f8f03d357505
Jothin Kumar <contact@jothin.tech>
2024-04-18 13:22:43 +0530

    fix typo

--- script/focus.js
+++ script/focus.js
@@ -19,7 +19,7 @@ function focusStart() {
     focusElem(window.mp.start.elem);
     const locater = document.getElementById("locate-animation")
     setTimeout(() => {
-        const elemRect = elem.getBoundingClientRect();
+        const elemRect = window.mp.start.elem.getBoundingClientRect();
         locater.style.top = `${(elemRect.top + elemRect.bottom - 200 - main.scrollTop + elemRect.height)/2}px`;
         locater.style.left = `${(elemRect.left + elemRect.right - 200 - main.scrollLeft + elemRect.width)/2}px`;
         locater.classList.add("locate-animation");
