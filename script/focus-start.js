eed2edd1ea239744a00572cb8882a03f692f0134
Jothin Kumar <contact@jothin.tech>
2024-04-18 10:37:39 +0530

    focus & locate start

--- script/focus-start.js
+++ script/focus-start.js
@@ -1,19 +1,17 @@
-// On portrait screens, autoscroll to extreme right if start isn't visible
-
-function elemInViewPort(elem) {
-    const rect = elem.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= window.innerHeight &&
-        rect.right <= window.innerWidth
-    );
-}
-
 function focusStart() {
     const main = document.getElementById("main");
-    main.scrollTo(0, 0);
-    if (!elemInViewPort(window.mp.start.elem)) {
-        main.scrollTo(1000, 0)
-    }
+    main.scrollTo(0, 0)
+    const start = window.mp.start.elem
+    setTimeout(() => {
+        start.scrollIntoView({block: "center", inline: "center"});
+    }, 200)
+    const locater = document.getElementById("locate-animation")
+    setTimeout(() => {
+        const startRect = start.getBoundingClientRect();
+        locater.style.top = `${(startRect.top + startRect.bottom - 200 - main.scrollTop + startRect.height)/2}px`;
+        locater.style.left = `${(startRect.left + startRect.right - 200 - main.scrollLeft + startRect.width)/2}px`;
+        locater.classList.add("locate-animation");
+    }, 1000)
 }
+
+window.addEventListener("resize", focusStart)
