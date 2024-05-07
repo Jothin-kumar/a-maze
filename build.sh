8e8c4bb1e3477053237e8ce8baff340e9b42d0d9
Jothin Kumar <contact@jothin.tech>
2024-05-07 18:12:36 +0530

    allow crawling

--- build.sh
+++ build.sh
@@ -13,5 +13,7 @@ printf "build.py ✅\n\n"
 
 cp root/robots.txt build-output/robots.txt
 printf "robots.txt ✅\n\n"
+cp root/sitemap.txt build-output/sitemap.txt
+printf "sitemap.txt ✅\n\n"
 cp -r root/assets build-output/assets
 printf "assets ✅\n\n"
