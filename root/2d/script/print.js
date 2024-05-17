d2f34dc4629b121b2c7077a65c9e4499ec6f5b50
Jothin Kumar <contact@jothin.tech>
2024-05-17 17:54:54 +0530

    fix typo

--- root/2d/script/print.js
+++ root/2d/script/print.js
@@ -18,6 +18,7 @@ window.addEventListener("beforeprint", async function() {
     window.addEventListener("afterprint", window.afterPrintEvtListener)
 
     const mazeID = await getMazeID(exportMaze(), window.currentLevel);
+    const url = `https://joth.in/maze?id=${mazeID}`;
     window.shareURL = url;
     document.getElementById("print-msg").innerText = url
 })
