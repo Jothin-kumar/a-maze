8957542447d37f1351292dcfbad0d96e765f4cec
Jothin Kumar <contact@jothin.tech>
2024-05-17 19:07:14 +0530

    Reveal answer from current location (#24)
    * navigate correct path from current location
    
    * fix paths partially not visible issue
    
    * do not retrace repeated path
    
    * do not display game over screen after revealing answer (bug fix)

--- root/2d/script/reveal.js
+++ root/2d/script/reveal.js
@@ -4,13 +4,14 @@ async function revealAnswer() {
     document.getElementById("reveal-answer").style.display = "none"
     document.getElementById("onscreen-nav").classList.remove("show-onscreen-nav")
 
-    player.reset()
-    for (let i = 0 ; i < mp.path.length ; i++) {
-        dot = mp.path[i]
-        if (![mp.start, mp.end].includes(dot)) {
-            window.player.moveBy(dot.x - player.x, dot.y - player.y)
-            await new Promise(r => setTimeout(r, 100))
-        }
+    const pathFromLocation = [
+        ...player.wayBackCorrectPath,
+        ...mp.path.slice(mp.path.indexOf(player.wayBackCorrectPath[player.wayBackCorrectPath.length - 1]) + 1)
+    ]
+    for (let i = 0 ; i < pathFromLocation.length ; i++) {
+        dot = pathFromLocation[i]
+        window.player.moveBy(dot.x - player.x, dot.y - player.y)
+        await new Promise(r => setTimeout(r, 100))
     }
     await new Promise(r => setTimeout(r, 200))
     window.player.currentElem.classList.remove("current-player")
@@ -18,4 +19,6 @@ async function revealAnswer() {
     mp.path.forEach((dot) => {
         dot.setColor("rgba(0, 50, 200, .5)");
     });
+    mp.start.setColor("rgba(0, 200, 0, .5)");
+    mp.end.setColor("rgba(200, 0, 0, .5)");
 }
