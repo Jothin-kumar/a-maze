9fb5d8b3ce2e376cec82c823e61d49527f9fefbb
Jothin Kumar <contact@jothin.tech>
2024-05-18 17:01:11 +0530

    consider no. of joint squares instead of lines

--- root/2d/script/difficulty.js
+++ root/2d/script/difficulty.js
@@ -1,17 +1,16 @@
-function movableNeighbours(dot) {
-    const neighbors = [];
-    function handler(x, y) {
-        let potentialMovableNeighbor = window.mazeSquares[`${dot.x+x},${dot.y+y}`];
-        if (potentialMovableNeighbor && canMove(dot.x, dot.y, dot.x + x, dot.y + y)) {
-            neighbors.push(potentialMovableNeighbor);
-        }
-    }
-    [[1, 0], [-1, 0] ,[0, 1], [0, -1]].forEach(([x, y]) => handler(x, y));
-    return neighbors;
-}
-
 function getDifficulty() {
     var difficulty = 0;
+    function movableNeighbours(dot) {
+        const neighbors = [];
+        function handler(x, y) {
+            let potentialMovableNeighbor = window.mazeSquares[`${dot.x+x},${dot.y+y}`];
+            if (potentialMovableNeighbor && canMove(dot.x, dot.y, dot.x + x, dot.y + y)) {
+                neighbors.push(potentialMovableNeighbor);
+            }
+        }
+        [[1, 0], [-1, 0] ,[0, 1], [0, -1]].forEach(([x, y]) => handler(x, y));
+        return neighbors;
+    }
     const correctPathList = Object.values(window.mp.path)
     for (let i = 0; i < correctPathList.length; i++) {
         let dot = correctPathList[i];
@@ -19,6 +18,25 @@ function getDifficulty() {
         difficulty += neighbors.length - 2;
     }
     
-    difficulty *= Object.values(lines).filter(l => l.hidden && !l.ignore).length / Object.values(lines).length;
+    let acceptedSquares = [...mp.path]
+    function ConnectConnectableSquares() {
+        connetables = []
+        acceptedSquares.forEach((sq) => {
+            movableNeighbours(sq).forEach((sq) => {
+                if (!acceptedSquares.includes(sq) && !connetables.includes(sq)) {
+                    connetables.push(sq)
+                }
+            })
+        })
+        acceptedSquares.push(...connetables)
+        return connetables.length > 0
+    }
+    let r = ConnectConnectableSquares()
+    while (r) {
+        r = ConnectConnectableSquares()
+    }
+
+    console.log(acceptedSquares.length / (gridSize**2));
+    difficulty *= acceptedSquares.length / (gridSize**2)
     return difficulty;
 }
