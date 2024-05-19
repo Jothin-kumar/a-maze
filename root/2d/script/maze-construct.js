aa7d0a3dd0ff6d30ff6d01d6c80a6f83db84cb2b
Jothin Kumar <contact@jothin.tech>
2024-05-19 17:36:28 +0530

    UI update

--- root/2d/script/maze-construct.js
+++ root/2d/script/maze-construct.js
@@ -194,7 +194,7 @@ function postConstruct() {
 
     mp.end.setColor("red");
     document.getElementById("main").style.display = "block"
-    document.getElementById("controls").style.display = "block"
+    document.getElementById("controls").style.display = "flex"
     document.getElementById("loading").style.display = "none"
     window.navNotAllowed = true
     startPos = window.mp.start;
