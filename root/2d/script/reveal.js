11ca169ef8fd9c09cc114076b6ba28ff21320898
Jothin-Kumar <contact@jothin.tech>
2024-05-31 21:37:32 +0530

    increase time

--- root/2d/script/reveal.js
+++ root/2d/script/reveal.js
@@ -11,7 +11,7 @@ async function revealAnswer() {
     for (let i = 0 ; i < pathFromLocation.length ; i++) {
         dot = pathFromLocation[i]
         window.player.moveBy(dot.x - player.x, dot.y - player.y)
-        await new Promise(r => setTimeout(r, 100))
+        await new Promise(r => setTimeout(r, 250))
     }
     await new Promise(r => setTimeout(r, 200))
     window.player.currentElem.classList.remove("current-player")
