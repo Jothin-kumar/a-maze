832797bfa10c286c06decdbe3aa18bfa35c8d422
Jothin Kumar <contact@jothin.tech>
2024-10-12 20:04:57 +0530

    Add accounts (#28)
    * Add profile page
    
    * Add home btn
    
    * add discord server
    
    * don't flash "Anonymous Player" to registered players
    
    * fix typo
    
    * add profile widget
    
    * Add z-index to profile widget
    
    * Add opacity to profile widget
    
    * Refactor profile widget styling and behavior

---
+++ root/script/profile-widget.js
@@ -0,0 +1,36 @@
+/* Mandatory to import connect.js before this */
+
+const pfp = document.createElement('img');
+pfp.id = 'profile-widget';
+const style = document.createElement('style');
+style.innerHTML = `
+#profile-widget {
+    border: 2px solid rgb(67, 35, 41);
+    border-radius: 5px;
+    position: fixed;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
+    z-index: 1000;
+    opacity: 0.9;
+    scale: 0.5;
+    transition: 0.5s;
+}
+#profile-widget:hover {
+    transform: rotate(5deg);
+    scale: 0.6;
+}
+`;
+document.body.appendChild(pfp);
+document.head.appendChild(style);
+pfp.onclick = function() {
+    window.open('/me', '_blank');
+}
+
+window.addEventListener('login', function(e) {
+    const details = getUserDetails()
+    pfp.src = details.photo;
+});
+window.addEventListener('logout', function(e) {
+    pfp.src = '/assets/img/unknown.png';
+});
