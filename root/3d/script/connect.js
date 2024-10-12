dade54defe62d3fb95a18263c212852b657174d4
Jothin Kumar <contact@jothin.tech>
2024-10-12 14:17:01 +0530

    Initiate 3D version

---
+++ root/3d/script/connect.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js"
+const firebaseConfig = {
+    apiKey: "AIzaSyCvmrpBQmuUWoRsBKpyrmgJRtPZ90ahA0U",
+    authDomain: "a-maze-jothinkumar.firebaseapp.com",
+    databaseURL: "https://a-maze-jothinkumar-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "a-maze-jothinkumar",
+    storageBucket: "a-maze-jothinkumar.appspot.com",
+    messagingSenderId: "222156193241",
+    appId: "1:222156193241:web:2e719f96af967accd32dc8",
+    measurementId: "G-SZ4H3YMY40"
+}
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+window.login = () => {
+    if (auth.currentUser) {
+        window.user = auth.currentUser;
+        window.dispatchEvent(new Event('login'));
+        return
+    }
+    signInWithPopup(auth, provider)
+    .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        window.user = result.user;
+        window.dispatchEvent(new Event('login'));
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+    });
+}
+window.logout = () => {
+    auth.signOut().then(() => {
+        console.log('Logged out');
+        window.user = null;
+        window.dispatchEvent(new Event('logout'));
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+window.getUserDetails = () => {
+    return {
+        name: window.user.displayName,
+        email: window.user.email,
+        photo: window.user.photoURL,
+        uid: window.user.uid,
+        lastLogin: window.user.metadata.lastSignInTime,
+        creationTime: window.user.metadata.creationTime
+    };
+}
