eca67e8635cd2df1e957899aca922db700d2c102
Jothin Kumar <contact@jothin.tech>
2024-10-12 20:00:05 +0530

    Refactor profile widget styling and behavior

--- root/script/profile-widget.js
+++ root/script/profile-widget.js
@@ -1,16 +1,28 @@
 /* Mandatory to import connect.js before this */
 
 const pfp = document.createElement('img');
-pfp.style.border = '2px solid rgb(67, 35, 41)';
-pfp.style.borderRadius = '5px';
-pfp.style.position = 'fixed';
-pfp.style.top = '10px';
-pfp.style.right = '10px';
-pfp.style.scale = '0.5';
-pfp.style.cursor = "pointer";
-pfp.style.zIndex = '1000';
-pfp.style.opacity = '0.9';
+pfp.id = 'profile-widget';
+const style = document.createElement('style');
+style.innerHTML = `
+#profile-widget {
+    border: 2px solid rgb(67, 35, 41);
+    border-radius: 5px;
+    position: fixed;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
+    z-index: 1000;
+    opacity: 0.9;
+    scale: 0.5;
+    transition: 0.5s;
+}
+#profile-widget:hover {
+    transform: rotate(5deg);
+    scale: 0.6;
+}
+`;
 document.body.appendChild(pfp);
+document.head.appendChild(style);
 pfp.onclick = function() {
     window.open('/me', '_blank');
 }
