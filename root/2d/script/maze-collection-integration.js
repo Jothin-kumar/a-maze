b17db109e26d65f4bd84b8b6533b2788b7f648be
Jothin Kumar <contact@jothin.tech>
2024-10-14 21:49:16 +0530

    bring back buttons if a new game is started after revealing answer of previous maze

--- root/2d/script/maze-collection-integration.js
+++ root/2d/script/maze-collection-integration.js
@@ -2,5 +2,8 @@ function playNewMaze() {
     document.getElementById("a-maze").style.opacity = "1"
     document.getElementById("maze-collection-integration").style.display = "none"
     document.getElementById("profile-widget").style.display = "block"
+    document.getElementById("maze-overlay").style.display = "inline-block"
+    document.getElementById("reset-btn").style.display = "inline-block"
+    document.getElementById("reveal-answer").style.display = "inline-block"
     A_Maze_main()
 }
