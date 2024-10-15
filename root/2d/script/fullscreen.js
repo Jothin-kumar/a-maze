2c7de8d23f4795a9ccfb4b6ff1fdcdc166822002
Jothin Kumar <contact@jothin.tech>
2024-10-15 07:49:27 +0530

    fix fullscreen

--- root/2d/script/fullscreen.js
+++ root/2d/script/fullscreen.js
@@ -5,11 +5,17 @@ fullscreenBtn.addEventListener("click", function() {
         fullscreenBtn.style.display = "none"
     })
 })
-document.addEventListener("fullscreenchange", function() {
-    if (!document.fullscreenElement) {
-        fullscreenBtn.style.display = "block"
+
+window.addEventListener("resize", function() { // https://stackoverflow.com/a/22662650/15923012
+    var maxHeight = window.screen.height,
+        maxWidth = window.screen.width,
+        curHeight = window.innerHeight,
+        curWidth = window.innerWidth;
+
+    if (maxWidth == curWidth && maxHeight == curHeight) {
+        fullscreenBtn.style.display = "none"
     }
     else {
-        fullscreenBtn.style.display = "none"
+        fullscreenBtn.style.display = "block"
     }
 })
