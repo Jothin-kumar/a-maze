5d488b8da9d58489b65a631b7bb1a27373c399cb
Jothin Kumar <contact@jothin.tech>
2024-10-15 08:48:27 +0530

    *

--- root/2d/script/fullscreen.js
+++ root/2d/script/fullscreen.js
@@ -18,3 +18,15 @@ function toNormal() {
 }
 
 window.fslistener = fullscreenBtn.addEventListener("click", toFullscreen)
+
+window.addEventListener("fullscreenchange", () => {
+    if (document.fullscreenElement) {
+        fullscreenBtn.innerHTML = '<span id="fullscreen-toggle" class="material-symbols-outlined">fullscreen_exit</span>'
+        fullscreenBtn.removeEventListener("click", toFullscreen)
+        fullscreenBtn.addEventListener("click", toNormal)
+    } else {
+        fullscreenBtn.innerHTML = '<span id="fullscreen-toggle" class="material-symbols-outlined">fullscreen</span>'
+        fullscreenBtn.removeEventListener("click", toNormal)
+        fullscreenBtn.addEventListener("click", toFullscreen)
+    }
+})
