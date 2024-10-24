ec11c83c4c2ad7633044b3b106fe7e9592e66680
Jothin Kumar <contact@jothin.tech>
2024-10-24 22:27:00 +0530

    Add click event listener for maze grid navigation

--- root/2d/script/screen-interactions.js
+++ root/2d/script/screen-interactions.js
@@ -7,3 +7,25 @@ navAssistBtn.addEventListener('click', function() {
         navAssistInit()
     }
 });
+
+document.getElementById("maze-grid").addEventListener('click', function(e) {
+    const navAssistBCR = navAssistBtn.getBoundingClientRect();  
+    const x = e.clientX - ((navAssistBCR.left + navAssistBCR.right)/2);
+    const y = e.clientY - ((navAssistBCR.top + navAssistBCR.bottom)/2);
+    if (Math.abs(x/y) > 2) {
+        if (x > 0) {
+            player.moveBy(1, 0)
+        }
+        else {
+            player.moveBy(-1, 0)
+        }
+    }
+    else if (Math.abs(y/x) > 2) {
+        if (y > 0) {
+            player.moveBy(0, 1)
+        }
+        else {
+            player.moveBy(0, -1)
+        }
+    }
+})
