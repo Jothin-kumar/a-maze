ff733b8a485f95a9f78f3d5691e173959b3c2450
Jothin Kumar <contact@jothin.tech>
2025-03-06 06:20:11 +0530

    Fix Improve help dialog style #57

--- root/2d/script/help.js
+++ root/2d/script/help.js
@@ -2,11 +2,13 @@ const help = document.getElementById("help-parent")
 function showHelp() {
     help.style.display = "block"
     window.navNotAllowed = true
+    main.style.opacity = "0.2"
     if (!isNavAssistHidden()) hideNavAssist("help")
 }
 function hideHelp() {
     help.style.display = "none"
     window.navNotAllowed = false
+    main.style.opacity = "1"
     if (window.navAssistHiddenBy === "help") displayNavAssist()
 }
 const isHelpOpen = () => help.style.display === "block"
