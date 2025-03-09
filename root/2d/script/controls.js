052277f1edb648e944ed87c4133b186b22f25730
Jothin Kumar <contact@jothin.tech>
2025-03-09 22:41:47 +0530

    add keyboard shortcut "c" key for switching controls visibility

--- root/2d/script/controls.js
+++ root/2d/script/controls.js
@@ -2,7 +2,7 @@ const hideControlsBtn = document.getElementById("hide-controls-btn")
 const hideControlsBtnSpan = document.querySelector("#hide-controls-btn > span")
 window.controlsHidden = false
 
-hideControlsBtn.addEventListener("click", () => {
+function switchControlVisibility() {
     if (window.controlsHidden) {
         hideControlsBtnSpan.innerHTML = "expand_circle_down"
         controls.style.display = "flex"
@@ -12,4 +12,10 @@ hideControlsBtn.addEventListener("click", () => {
         controls.style.display = "none"
     }
     window.controlsHidden = !window.controlsHidden
+}
+hideControlsBtn.addEventListener("click", switchControlVisibility)
+document.addEventListener("keydown", (e) => {
+    if (["c", "C"].includes(e.key)) {
+        switchControlVisibility()
+    }
 })
