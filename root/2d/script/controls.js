842da526cceaad5f03a7e87df99d437c9c929ea6
Jothin Kumar <contact@jothin.tech>
2025-03-17 10:41:07 +0530

    style change for controls

--- root/2d/script/controls.js
+++ root/2d/script/controls.js
@@ -1,18 +1,18 @@
 const hideControlsBtn = document.getElementById("hide-controls-btn")
-const hideControlsBtnSpan = document.querySelector("#hide-controls-btn > span")
+const showControlsBtn = document.getElementById("show-controls-btn")
 window.controlsHidden = false
 
 function switchControlVisibility() {
     const fullscreenBtn = document.getElementById("fullscreen-btn")
     const profileWidget = document.getElementById("profile-widget")
     if (window.controlsHidden) {
-        hideControlsBtnSpan.innerHTML = "expand_circle_down"
-        controls.style.display = "flex"
+        showControlsBtn.style.opacity = 0
+        controls.style.display = "block"
         fullscreenBtn.style.display = "inline-block"
         profileWidget.style.display = "block"
     }
     else {
-        hideControlsBtnSpan.innerHTML = "expand_circle_up"
+        showControlsBtn.style.opacity = 1
         controls.style.display = "none"
         fullscreenBtn.style.display = "none"
         profileWidget.style.display = "none"
@@ -21,8 +21,11 @@ function switchControlVisibility() {
     adjustNavAssistPosition()
 }
 hideControlsBtn.addEventListener("click", switchControlVisibility)
+showControlsBtn.addEventListener("click", switchControlVisibility)
 document.addEventListener("keydown", (e) => {
     if (["c", "C"].includes(e.key)) {
+        console.log("Switching control visibility");
+        
         switchControlVisibility()
     }
 })
