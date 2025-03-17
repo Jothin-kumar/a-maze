074989c364acbf65cd834945607ec67d3481197b
Jothin Kumar <contact@jothin.tech>
2025-03-17 07:03:22 +0530

    When user goes full screen, hide controls automatically

--- root/2d/script/fullscreen.js
+++ root/2d/script/fullscreen.js
@@ -10,6 +10,7 @@ function toFullscreen() {
     fullscreenBtn.removeEventListener("click", toFullscreen)
     fullscreenBtn.addEventListener("click", toNormal)
     fullscreenNotice.style.display = "none"
+    if (!window.controlsHidden) switchControlVisibility()
 }
 function toNormal() {
     document.exitFullscreen()
