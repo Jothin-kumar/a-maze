6861c03e00bea3c0432fa699edae0c71d84cb4c1
Jothin Kumar <contact@jothin.tech>
2025-05-14 16:19:40 +0530

    Integrate all profile operations into profile widget

--- root/script/profile-widget.js
+++ root/script/profile-widget.js
@@ -1,36 +1,112 @@
 /* Mandatory to import connect.js before this */
 
 const pfp = document.createElement('img');
-pfp.id = 'profile-widget';
+pfp.id = 'pfp';
 const style = document.createElement('style');
 style.innerHTML = `
-#profile-widget {
+#pfp {
     border: 2px solid rgb(67, 35, 41);
     border-radius: 5px;
     position: fixed;
-    top: calc(5px + 100dvh - 100dvh);
-    right: calc(5px + 100dvw - 100dvw);
+    top: 5px;
+    right: 5px;
     cursor: pointer;
     z-index: 1000;
     opacity: 0.69;
     scale: 0.5;
     transition: 0.5s;
 }
-#profile-widget:hover {
+#pfp:hover {
     transform: rotate(5deg);
     scale: 0.6;
 }
+#profile-card {
+    position: fixed;
+    top: 80px;
+    right: 5px;
+    z-index: 1000;
+    display: none;
+    text-align: right;
+    font-family: 'Courier New', Courier, monospace;
+    background-color: black;
+    padding: 10px;
+    border-radius: 5px;
+}
+#user-name {
+    font-size: 1.5em;
+    font-weight: bold;
+}
+#user-id {
+    font-size: .8em;
+    color: #888f;
+}
+#user-reg-since {
+    font-size: .8em;
+    color: #8888;
+}
+.pfp-button {
+    flex-grow: 1;
+    padding: 10px;
+    margin: 5px;
+    font-size: 1rem;
+    background-color: black;
+    color: white;
+    border: 1px solid rgb(63, 67, 35);
+    border-radius: 5px;
+    cursor: pointer;
+    transition: 1s;
+    opacity: .8;
+}
+.pfp-button:hover {
+    border: 2px solid olive;
+    opacity: 1;
+}
+.pfp-button.login:hover {
+    border: 2px solid green;
+    background-color: green;
+    opacity: 1;
+}
+.pfp-button.logout:hover {
+    border: 2px solid red;
+    background-color: red;
+    opacity: 1;
+}
+`;
+const profileCard = document.createElement('div');
+profileCard.id = 'profile-card';
+profileCard.innerHTML = `
+<p id="user-name"></p>
+<p id="user-id"></p>
+<p id="user-reg-since"></p>
+<button class="pfp-button login" onclick="login()" id="login-btn">Sign Up / Log In</button>
+<button class="pfp-button logout" onclick="logout()" id="logout-btn" style="display: none;">Logout</button>
 `;
 document.body.appendChild(pfp);
+document.body.appendChild(profileCard);
 document.head.appendChild(style);
 pfp.onclick = function() {
-    window.open('/me', '_blank');
+    document.getElementById('profile-card').style.display = 'block';
+}
+document.body.onclick = function(event) {
+    if (event.target !== pfp && event.target !== profileCard && event.target.parentNode !== profileCard) {
+        document.getElementById('profile-card').style.display = 'none';
+    }
 }
 
 window.addEventListener('login', function(e) {
     const details = getUserDetails()
     pfp.src = details.photo;
+    document.getElementById('user-name').innerText = details.name;
+    document.getElementById('user-id').innerText = details.uid;
+    document.getElementById('user-reg-since').innerText = `Registered A-Maze player since ${details.creationTime}`;
+    document.getElementById('login-btn').style.display = 'none';
+    document.getElementById('logout-btn').style.display = 'block';
 });
 window.addEventListener('logout', function(e) {
     pfp.src = '/assets/img/unknown.png';
+    document.getElementById('user-name').innerText = 'Anonymous Player';
+    document.getElementById('user-id').innerText = '00000000000000000000000';
+    document.getElementById('user-reg-since').innerText = 'Registered A-Maze player since... Now?';
+    document.getElementById('login-btn').style.display = 'block';
+    document.getElementById('logout-btn').style.display = 'none';
 });
