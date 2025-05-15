eb4d39774ad69bb6b13d8c72190791d55f8b08bf
Jothin Kumar <contact@jothin.tech>
2025-05-15 14:20:16 +0530

    more clarity

--- root/2d/script/guide.js
+++ root/2d/script/guide.js
@@ -78,27 +78,29 @@ function showGuide() {
     window.guideShowing = true;
     window.navNotAllowed = true;
     document.getElementById("guide-btn").style.opacity = 0;
+    const navAssistBtn = document.getElementById("nav-assist-btn");
+    navAssistBtn.style.display = "none";
     setTimeout(() => {
         setZoom(window.zoom*2);
         window.mp.end.elem.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
         showGuideTooltip(window.mp.end.elem, "This is your goal. Click for next", () => {
             window.player.currentElem.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
             showGuideTooltip(window.player.currentElem, "You're here.", async () => {
-                showGuideTooltip(document.getElementById("nav-assist-btn"),
-                    isTouchDevice ? "Click above, below, left or right to move." : "Use arrow keys or WASD to move.",
+                targetZoom = window.zoom/2;
+                while (targetZoom < window.zoom) {
+                    setZoom(window.zoom - .1);
+                    if (window.zoom - targetZoom < .1) {
+                        setZoom(targetZoom);
+                        break;
+                    }
+                    await new Promise(r => setTimeout(r, 1));
+                }
+                window.navNotAllowed = false
+                navAssistBtn.style.display = "block";
+                window.player.currentElem.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+                showGuideTooltip(navAssistBtn,
+                    isTouchDevice ? "Click above, below, left or right with respect to the white circle to move." : "Use arrow keys or WASD to move.",
                     async () => {
-                        targetZoom = window.zoom/2;
-                        while (targetZoom < window.zoom) {
-                            setZoom(window.zoom - .1);
-                            if (window.zoom - targetZoom < .1) {
-                                setZoom(targetZoom);
-                                break;
-                            }
-                            await new Promise(r => setTimeout(r, 1));
-                        }
-                        window.player.currentElem.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
-                        showGuideTooltip(window.player.currentElem, "You're all set!")
-                        window.navNotAllowed = false
                         window.guideShowing = false;
                         document.getElementById("guide-btn").style.opacity = 1;
                     }
